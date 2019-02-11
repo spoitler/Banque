20647c11689f77493c756d2172b4c09a18750920
Thomas <thomas.salvignol@epsi.fr>
2019-02-11 13:37:31 +0100

    Reset du git

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace banque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compte c1 = new Compte();
+            c1.Id = 2;
+            c1.Libelle = "Intermarché";
+            c1.Type ="Compte courant";
+            c1.Plafond = 12000;
+            c1.Solde = 1548;
+
+            Client cc1 = new Client();
+
+            Console.WriteLine(c1.Description);
+            
+        }
+    }
+}
+
+
+
---
+++ class/Client.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace banque
+{
+    class Client
+    {
+        private int idClient;
+        private string nom;
+        private string prenom;
+        private string adresse1;
+        private string adresse2;
+        private string codeP;
+        private string ville;
+        private string telephone;
+
+        public Client()
+        {
+            this.idClient = idClient;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.adresse1 = adresse1;
+            this.adresse2 = adresse2;
+            this.codeP = codeP;
+            this.ville = ville;
+            this.telephone = telephone;
+        }
+    }
+}
+
